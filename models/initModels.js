38d833f4c545dd32c005d47e66e285432bada830
Vianey Torres Cabrera <vian19_tc@hotmail.com>
2022-10-05 20:49:19 -0500

    first commit

---
+++ models/initModels.js
@@ -0,0 +1,50 @@
+const { User } = require('./user.model');
+const { Product } = require('./product.model');
+const { ProductImgs } = require('./productImgs.model');
+const { Category } = require('./categories.model');
+const { ProductInCart } = require('./productInCart.model');
+const { Cart } = require('./cart.model');
+const { Order } = require('./order.model');
+
+
+
+const initModels = () => {
+	// 1 User <----> M Products
+	User.hasMany(Product, { foreignKey: 'userId' });
+	Product.belongsTo(User);
+
+	// 1 User <----> M Orders
+	User.hasMany(Order, { foreignKey: 'userId' });
+	Order.belongsTo(User);
+
+	// 1 user <-----------> 1 cart
+	User.hasOne(Cart, {
+	foreignKey: 'userId'
+	});
+	Cart.belongsTo(User);
+
+	// 1 cart <-----------> 1 order
+	Cart.hasOne(Order, {
+		foreignKey: 'cartId'
+	});
+	Order.belongsTo(Cart);
+
+	// 1 Cart <----> M Products
+	Cart.hasMany(ProductInCart, { foreignKey: 'cartId' });
+	ProductInCart.belongsTo(Cart);
+
+	// 1 Product <----> M Imgs
+	Product.hasMany(ProductImgs, { foreignKey: 'productId' });
+	ProductImgs.belongsTo(Product);
+
+	// 1 product <-----------> 1 category
+	Category.hasOne(Product);
+	Product.belongsTo(Category);
+
+	// 1 products in Cart <----> M ProductsImgs*
+	ProductInCart.hasMany(ProductImgs, { foreignKey: 'productId' });
+	ProductImgs.belongsTo(ProductInCart);
+
+};
+
+ module.exports = { initModels };
